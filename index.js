3816c353e7effb5c782a85f923f0105a0d58327b
Zain1306 <zulabdin795@gmail.com>
2022-08-11 19:47:05 +0530

    new

--- index.js
+++ index.js
@@ -7,23 +7,39 @@ const mongoose = require('mongoose');
 
 
 // Basic Configuration
-
+const port = 3000;
 
 async function conndb()
 {
+
   try {
     await mongoose.connect(process.env.PORT, {useNewUrlParser: true, useUnifiedTopology: true});
 //Get the default connection
 var db  = await mongoose.connection;
 console.log("Database connected succsefully");
   } catch (error) {
-    db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+    console.log('MongoDB connection error:'); 
   }
 
 //Bind connection to error event (to get notification of connection errors)
 }
 
 console.log(conndb());
+
+
+
+app.get('/api/shorturl/:input', (request, response) => {
+  let input = request.params.input
+  
+  Url.findOne({short: input}, (error, result) => {
+    if(!error && result != undefined){
+      response.redirect(result.original)
+    }else{
+      response.json('URL not Found')
+    }
+  })
+})
+
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -40,3 +56,50 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
+let urlSchema = new mongoose.Schema({
+  original : {type: String, required: true},
+  short: Number
+})
+
+let Url = mongoose.model('Url', urlSchema)
+
+let bodyParser = require('body-parser')
+let responseObject = {}
+app.post('/api/shorturl', bodyParser.urlencoded({ extended: false }) , (request, response) => {
+  let inputUrl = request.body['url']
+  
+  let urlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi)
+  
+  if(!inputUrl.match(urlRegex)){
+    response.json({error: 'Invalid URL'})
+    return
+  }
+    
+  responseObject['original_url'] = inputUrl
+  
+  let inputShort = 1
+  
+  Url.findOne({})
+        .sort({short: 'desc'})
+        .exec((error, result) => {
+          if(!error && result != undefined){
+            inputShort = result.short + 1
+          }
+          if(!error){
+            Url.findOneAndUpdate(
+              {original: inputUrl},
+              {original: inputUrl, short: inputShort},
+              {new: true, upsert: true },
+              (error, savedUrl)=> {
+                if(!error){
+                  responseObject['short_url'] = savedUrl.short
+                  response.json(responseObject)
+                }
+              }
+            )
+          }
+  })
+  
+})
